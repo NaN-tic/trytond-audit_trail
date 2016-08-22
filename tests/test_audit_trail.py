03177972a4ee765d8cf41aabaebdb8cc43ee20b6
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-08-22 10:50:04 +0200

    Upgrade test to version 4.0

--- tests/test_audit_trail.py
+++ tests/test_audit_trail.py
@@ -2,50 +2,41 @@
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
-from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT
+from trytond.pool import Pool
+from trytond.tests.test_tryton import ModuleTestCase, with_transaction
 from trytond.transaction import Transaction
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
+    module = 'audit_trail'
 
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('audit_trail')
-        self.user = POOL.get('res.user')
-        self.session = POOL.get('ir.session')
-        self.event = POOL.get('ir.session.event')
-
-    def test0005views(self):
-        'Test views'
-        test_view('audit_trail')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
-
+    @with_transaction()
     def test_session_events(self):
-        with Transaction().start(DB_NAME, USER, CONTEXT) as transaction:
-            user, = self.user.create([{
-                        'name': 'Test User',
-                        'login': 'test',
-                        'password': '123456',
-                        }])
-            user_id = user.id
-            with transaction.set_user(user_id):
-                session, = self.session.create([{}])
-                key = session.key
-                event, = self.event.search([('key', '=', key)])
-                self.assertEqual(event.key, key)
-                self.assertEqual(event.user, user)
-                self.assertIsNotNone(event.login)
-                self.assertEqual(event.login, event.create_date)
-                self.assertIsNone(event.logout)
-                self.session.delete([session])
-                event, = self.event.search([('key', '=', key)])
-                self.assertEqual(event.login, event.create_date)
-                self.assertIsNotNone(event.logout)
-                self.assertEqual(event.logout, event.write_date)
+        pool = Pool()
+        User = pool.get('res.user')
+        Session = pool.get('ir.session')
+        Event = pool.get('ir.session.event')
+        user, = User.create([{
+                    'name': 'Test User',
+                    'login': 'test',
+                    'password': '123456',
+                    }])
+        user_id = user.id
+        with Transaction().set_user(user_id):
+            session, = Session.create([{}])
+            key = session.key
+            event, = Event.search([('key', '=', key)])
+            self.assertEqual(event.key, key)
+            self.assertEqual(event.user, user)
+            self.assertIsNotNone(event.login)
+            self.assertEqual(event.login, event.create_date)
+            self.assertIsNone(event.logout)
+            Session.delete([session])
+            event, = Event.search([('key', '=', key)])
+            self.assertEqual(event.login, event.create_date)
+            self.assertIsNotNone(event.logout)
+            self.assertEqual(event.logout, event.write_date)
 
 
 def suite():
