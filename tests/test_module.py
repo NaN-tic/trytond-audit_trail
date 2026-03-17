f657feff71f500d2a3f8f06e43f462062ac5c566
Juanjo Garcia <juanjo.garcia@nan-tic.com>
2026-03-17 11:57:58 +0100

    Add missing cron to be able of send a notification with the logs. Prepare corn with electronic mail, checking when a user surprass the total of records configured. Reorganize the audit menu entries. Add a "form_field" that let us decide when the uses only acces a tree view or the user acces a form view. Add tests Locales
    Task #186074

--- tests/test_module.py
+++ tests/test_module.py
@@ -2,6 +2,10 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 
+from datetime import datetime, timedelta
+from types import SimpleNamespace
+
+from trytond.modules.audit_trail import log as audit_trail_log
 from trytond.pool import Pool
 from trytond.tests.test_tryton import ModuleTestCase, with_transaction
 from trytond.transaction import Transaction
@@ -37,6 +41,88 @@ class AuditTrailTestCase(ModuleTestCase):
             self.assertEqual(event.login, event.create_date)
             self.assertIsNotNone(event.logout)
             self.assertEqual(event.logout, event.write_date)
+
+    @with_transaction()
+    def test_request_contains_field_helper(self):
+        request_data = {'params': [[1], ['name', 'login']]}
+
+        self.assertTrue(audit_trail_log._request_contains_field(
+                request_data, 'login'))
+        self.assertFalse(audit_trail_log._request_contains_field(
+                request_data, 'email'))
+
+    @with_transaction()
+    def test_get_over_limit_by_user(self):
+        pool = Pool()
+        Log = pool.get('audit_trail.log')
+        Model = pool.get('ir.model')
+        User = pool.get('res.user')
+
+        model, = Model.search([('model', '=', 'res.user')], limit=1)
+        user1, user2 = User.create([{
+                    'name': 'User One',
+                    'login': 'user_one',
+                    'password': 'NaN-123456',
+                    }, {
+                    'name': 'User Two',
+                    'login': 'user_two',
+                    'password': 'NaN-123456',
+                    }])
+        logs = Log.create([{
+                    'user': user1.id,
+                    'type': 'model',
+                    'operation': 'read',
+                    'record_ids': '[0]',
+                    'request': '{}',
+                    'model': model.id,
+                    }, {
+                    'user': user1.id,
+                    'type': 'model',
+                    'operation': 'read',
+                    'record_ids': '[1]',
+                    'request': '{}',
+                    'model': model.id,
+                    }, {
+                    'user': user1.id,
+                    'type': 'model',
+                    'operation': 'read',
+                    'record_ids': '[2]',
+                    'request': '{}',
+                    'model': model.id,
+                    }, {
+                    'user': user2.id,
+                    'type': 'model',
+                    'operation': 'read',
+                    'record_ids': '[3]',
+                    'request': '{}',
+                    'model': model.id,
+                    }, {
+                    'user': user1.id,
+                    'type': 'model',
+                    'operation': 'write',
+                    'record_ids': '[4]',
+                    'request': '{}',
+                    'model': model.id,
+                    }])
+        cursor = Transaction().connection.cursor()
+        table = Log.__table__()
+        cursor.execute(*table.update(
+                columns=[table.create_date],
+                values=[datetime.now() - timedelta(days=2)],
+                where=table.id == logs[0].id))
+
+        configuration = SimpleNamespace(models=[SimpleNamespace(
+                    number=1,
+                    model=model,
+                    operation='read',
+                    )])
+        over_limit = Log.get_over_limit_by_user(configuration)
+
+        self.assertEqual(len(over_limit), 1)
+        user, config_model, count = over_limit[0]
+        self.assertEqual(user, user1)
+        self.assertEqual(config_model.model, model)
+        self.assertEqual(count, 2)
 
 
 del ModuleTestCase
